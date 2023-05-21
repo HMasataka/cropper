ea60a846833edd1fefd9d421efd73ca48b33143c
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2023-05-21 13:27:57 +0900

    Feat crop image

--- src/components/cropper.tsx
+++ src/components/cropper.tsx
@@ -1,11 +1,12 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
+import { jsx, css } from "@emotion/react";
 
 import React, { useState, useEffect, useCallback } from "react";
 
-import { jsx, css } from "@emotion/react";
-
 import Cropper, { Area } from "react-easy-crop";
+
+import getCroppedImg from "scripts/getCroppedImg";
 
 const imageMimeType = /image\/(png|jpg|jpeg)/i;
 
@@ -16,14 +17,26 @@ const CropComponent: React.FC<CropComponentProps> = ({}) => {
   const [zoom, setZoom] = useState(1);
   const [file, setFile] = useState<File>();
   const [image, setImage] = useState<string>("");
+  const [croppedImage, setCroppedImage] = useState<string>("");
   const [openState, setOpenState] = useState<boolean>(false);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
 
   const onCropComplete = useCallback(
     (croppedArea: Area, croppedAreaPixels: Area) => {
-      console.log(croppedArea, croppedAreaPixels);
+      setCroppedAreaPixels(croppedAreaPixels);
     },
     []
   );
+
+  const showCroppedImage = useCallback(async () => {
+    if (!croppedAreaPixels) return;
+    try {
+      const croppedImage = await getCroppedImg(image, croppedAreaPixels);
+      setCroppedImage(croppedImage);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [croppedAreaPixels, image]);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files == null || e.target.files.length == 0) {
@@ -66,18 +79,38 @@ const CropComponent: React.FC<CropComponentProps> = ({}) => {
     <>
       <input type="file" accept=".png, .jpg, .jpeg" onChange={changeHandler} />
       {openState && (
-        <Cropper
-          image={image}
-          crop={crop}
-          zoom={zoom}
-          aspect={4 / 3}
-          onCropChange={setCrop}
-          onCropComplete={onCropComplete}
-          onZoomChange={setZoom}
-        />
+        <div css={cropContainer}>
+          <Cropper
+            image={image}
+            crop={crop}
+            zoom={zoom}
+            aspect={4 / 3}
+            onCropChange={setCrop}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+          />
+        </div>
+      )}
+      <button onClick={showCroppedImage} css={btn}>
+        AAA
+      </button>
+      {croppedImage && (
+        <img css={croppedImageStyle} src={croppedImage} alt="" />
       )}
     </>
   );
 };
 
+const btn = css``;
+
+const cropContainer = css`
+  position: relative;
+  width: 100%;
+  background-color: #333;
+  height: 200px;
+`;
+
+const croppedImageStyle = css`
+  max-height: 300px;
+`;
 export default CropComponent;
